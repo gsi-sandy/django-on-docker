179cb688adea7e704660ef002695dd3d44e9e492
gsi-sandy <sandy@generalsoftwareinc.com>
2020-07-01 01:47:50 -0400

    Create Polls application.

--- app/hello_django/settings.py
+++ app/hello_django/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
